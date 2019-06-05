070ef3600d52f31910ecefdb30b8cb30e7ceac3f
alexmold <a.moldavsky@atompark.com>
2019-06-05 12:19:47 +0300

    hw11 updated

--- src/hw11/hw11.js
+++ src/hw11/hw11.js
@@ -7,27 +7,14 @@ for (let i = 0; i < lightsList.length; i++) {
 }
 
 function lightsLampSetClick(lights) {
-    const red = lights.querySelector('.lights__lamp_red');
-    const yellow = lights.querySelector('.lights__lamp_yellow');
-    const green = lights.querySelector('.lights__lamp_green');
+    const lampsList = lights.querySelectorAll('.lights__lamp');
+    for (let i = 0; i < lampsList.length; i++) {
+        lampsList[i].addEventListener('click', () => {
+            for (let j = 0; j < lampsList.length; j++) {
+                lampsList[j].classList.remove('active');
+            }
+            lampsList[i].classList.add('active');
 
-    red.addEventListener('click', () => {
-        red.classList.add('lights__lamp_red_active'); 
-        yellow.classList.remove('lights__lamp_yellow_active');
-        green.classList.remove('lights__lamp_green_active');
-    });
-
-    yellow.addEventListener('click', () => {
-        red.classList.remove('lights__lamp_red_active'); 
-        yellow.classList.add('lights__lamp_yellow_active');
-        green.classList.remove('lights__lamp_green_active');
-    });
-
-    green.addEventListener('click', () => {
-        red.classList.remove('lights__lamp_red_active'); 
-        yellow.classList.remove('lights__lamp_yellow_active');
-        green.classList.add('lights__lamp_green_active');
-    });
-
-
+        });
+    }
 }
